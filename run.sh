182b3c53d3672bc576ee485023ccd343b176d1a7
Haaziq386 <haaziqjamal@gmail.com>
2025-02-18 23:33:21 +0400

    changed tests

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-EXECUTABLE="bin/lexer"
-TEST_DIR="tests"
+if [ $# -eq 0 ]; then
+  echo "Error: No test directory provided."
+  echo "Usage: $0 <test_directory>"
+  exit 1
+fi
+
+EXECUTABLE="bin/parser"
+TEST_DIR=$1
 OUTPUT_DIR="outputs"
 
 # Ensure test directory exists
@@ -16,18 +22,20 @@ mkdir -p "$OUTPUT_DIR"
 # Run the executable over each test case
 for test_case in "$TEST_DIR"/*.cold; do
   if [ -f "$test_case" ]; then
-    # Get the base name of the test case (without extension)
-    test_name=$(basename "$test_case" .cold)
 
+    test_name=$(basename "$test_case" .cold)
     echo "Running test case: $test_name"
 
-    "$EXECUTABLE" "$test_case" "$OUTPUT_DIR/$test_name.txt"
+    "$EXECUTABLE" "$test_case" "-o" "$OUTPUT_DIR/" "-a" "-f"
 
     if [ $? -eq 0 ]; then
-      echo "Test $test_name passed. Output saved to $OUTPUT_DIR/$test_name.txt"
+      echo "Test $test_name passed. Output saved in $OUTPUT_DIR/."
+      dot -Tpng "$OUTPUT_DIR/$test_name""_AST.dot" -o "$OUTPUT_DIR/$test_name""_ast.png"
+      rm "$OUTPUT_DIR/$test_name""_AST.dot"
     else
       echo "Test $test_name failed."
     fi
+  
   else
     echo "No test cases found in '$TEST_DIR'."
   fi
