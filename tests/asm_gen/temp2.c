4443f0e2a184a5a9c9d51e3c69dfaa873b32d11b
Shubham Kumar Verma <shubham_kv@cs.iitr.ac.in>
2025-04-16 11:06:16 +0530

    add pointer subtr, add support for array of int, char, float, struct and class, also fixed array element modification

--- tests/asm_gen/temp2.c
+++ tests/asm_gen/temp2.c
@@ -1,15 +1,52 @@
-int main(){
-    int x = 0;
-    int y = 5;
-    int z = 10;
-    y = z + x;
-    x = z + y;
-    z = y + x;
-    x = y + x;
-    y = y + z;
-    z = y + x;
-    y = y + z;
+struct node{
+    int x;
+    int y;
+};
+
+class cls{
+    public{
+    int x;
+    };
+};
+
+
+void f(){
+    // int x;char y;float z; struct node w; class cls v;
+    // int a[2] = {1+2,y};
+    // char b[2] = {'a','b'};
+    // float c[2] = {1.0,1};
+    // struct node d[2] = {w,w};
+    // class cls e[2] = {v,v};
+    // a[1] = x;
+    // b[1] = 'c';
+    // c[1] = z;
+    // d[1] = w;
+    // e[1] = v;
+    
+    // x = a[1];
+    // y = b[1];
+    
+    int *p; char *q;
+    struct node* r; class cls *s;
+    p = p + 1;
+    q = q - 1;
+    r = r - 1;
+    r -= 1;
+    s += 1; 
 }
+
+// int main(){
+//     int x = 0;
+//     int y = 5;
+//     int z = 10;
+//     y = z + x;
+//     x = z + y;
+//     z = y + x;
+//     x = y + x;
+//     y = y + z;
+//     z = y + x;
+//     y = y + z;
+// }
 
 // void foo(int x){
 
